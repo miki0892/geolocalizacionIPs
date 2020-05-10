7f3b60080cb7ed68b0308ce687d447222d220033
Miguel Pérez <miguelangelperez909@gmail.com>
2020-05-10 20:16:33 -0300

    Creo el proyecto

---
+++ symfony/var/cache/prod/twig/eb/ebe5521c760d1ee79104ad005edc675d807650ced127c3a76c02bdc8f354ad35.php
@@ -0,0 +1,75 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* lucky/number.html.twig */
+class __TwigTemplate_ac9c5c845c725b67f2f166a2b96eaaaba563c24fc66280c2f6eccd4d2b6d4c39 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 2
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $this->parent = $this->loadTemplate("base.html.twig", "lucky/number.html.twig", 2);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 4
+    public function block_body($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 5
+        echo "<h1>Your lucky number is ";
+        echo twig_escape_filter($this->env, ($context["number"] ?? null), "html", null, true);
+        echo "</h1>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "lucky/number.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  50 => 5,  46 => 4,  35 => 2,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "lucky/number.html.twig", "/var/www/symfony/templates/lucky/number.html.twig");
+    }
+}
